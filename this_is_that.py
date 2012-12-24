dda6323c10efc7d9803bfc3330e790226eacffbb
Austin Happel <austin@austinhappel.com>
2012-12-23 23:13:17 -0800

    added some flavor, added send commands instead of console prints

--- this_is_that.py
+++ this_is_that.py
@@ -4,6 +4,8 @@ from errbot import BotPlugin, botcmd  # Note: if you use a version < 1.6.0 see b
 import re
 import string
 import config
+from random import randint
+
 # import string
 
 
@@ -22,6 +24,20 @@ class ThisIsThat(BotPlugin):
         return r'[' + self.regex_punctuation + r']*' + self.my_short_name + \
         r'[' + self.regex_punctuation + r']*'
 
+    def get_answer_flavor_text(self):
+        flavors = [
+            'I have been told that ',
+            'Someone once said that ',
+            'I think ',
+            'I believe ',
+            ''
+        ]
+
+        return flavors[randint(0, len(flavors) - 1)]
+
+    def get_sender_short_name(self, sender):
+        return sender[0:sender.find('@')]
+
     # @botcmd(split_args_with=' ')
     def parse_command(self, mess):
         """ Looks for 'is' and saves XXXX is XXXX into database"""
@@ -36,7 +52,7 @@ class ThisIsThat(BotPlugin):
         self.__setitem__(this_that[0].strip().strip(strip_characters), \
             this_that[1].strip().strip(strip_characters))
 
-        print "Whatever you say, " + sender + "."
+        return "Whatever you say, " + self.get_sender_short_name(sender) + "."
 
     def parse_question(self, mess, must_respond=False):
         """ Searches the db for a key whose name comes after the 'is'
@@ -54,10 +70,9 @@ class ThisIsThat(BotPlugin):
         key = message_clean.strip().strip(strip_characters)
 
         if key in self.keys():
-            print key + ' is ' + self.__getitem__(key)
+            return self.get_answer_flavor_text() + key + ' is ' + self.__getitem__(key)
         elif must_respond is True:
-            print "I don't know what " + key + " is."
-
+            return "I don't know what " + key + " is."
 
     # TODO: what if name is stated twice in message
     def callback_message(self, conn, mess):
@@ -66,18 +81,24 @@ class ThisIsThat(BotPlugin):
         # is my name mentioned?
         if re.search(self.short_name_regex, message) != None:
             # is this a command?
-            if re.search(self.question_regex, message) == None and \
+            if re.search(self.question_regex, message) is None and \
             re.search(r'\bis\b', message) != None:
-                self.parse_command(mess)
+                response = self.parse_command(mess)
+                if response != None:
+                    self.send(mess.getFrom(), response, message_type=mess.getType())
 
             # is this a request?
             if re.search(self.question_regex, message) != None:
-                self.parse_question(mess, True)
+                response = self.parse_question(mess, True)
+                if response != None:
+                    self.send(mess.getFrom(), response, message_type=mess.getType())
 
         else:
             # is this a request?
             if re.search(self.question_regex, message) != None:
-                self.parse_question(mess)
+                response = self.parse_question(mess)
+                if response != None:
+                    self.send(mess.getFrom(), response, message_type=mess.getType())
 
 #        if emoticonCommand:
 #            key = emoticonCommand.group(1)
