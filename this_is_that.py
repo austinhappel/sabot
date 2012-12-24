b8af2b70dc7613a1b6fd0a67feef0a1b978580e7
Austin Happel <austin@austinhappel.com>
2012-12-23 22:42:51 -0800

    added basic parroting

--- this_is_that.py
+++ this_is_that.py
@@ -2,32 +2,82 @@
 
 from errbot import BotPlugin, botcmd  # Note: if you use a version < 1.6.0 see below.
 import re
+import string
 import config
+# import string
 
 
 class ThisIsThat(BotPlugin):
 
+    my_full_name = config.BOT_IDENTITY['username']
+    regex_punctuation = r'!"#$%&\'()*+,-./:;<=>?@\[\\\]^_`{|}~\s'
+    question_regex = r'\b(what|who) is\b'
+
+    @property
+    def my_short_name(self):
+        return self.my_full_name[0:self.my_full_name.find('@')]
+
+    @property
+    def short_name_regex(self):
+        return r'[' + self.regex_punctuation + r']*' + self.my_short_name + \
+        r'[' + self.regex_punctuation + r']*'
+
     # @botcmd(split_args_with=' ')
-    def parseThisThat(self, message):
+    def parse_command(self, mess):
         """ Looks for 'is' and saves XXXX is XXXX into database"""
 
-        if len(args) == 2:
-            self.__setitem__(str(args[0]), str(args[1]))
-            return 'saved ((' + args[0] + ')) as ' + args[1] + '.'
+        message = str(mess.getBody())
+        sender = str(mess.getFrom())
+        strip_characters = string.punctuation + ' '
+        cleaned_message = re.sub(self.short_name_regex, '', message)
+        this_that = re.split(r'\bis\b', cleaned_message, maxsplit=1)
 
-        return 'usage: `!emoticon word http://url`'
+        # save to the database, strip whitespace and punctuation from both
+        self.__setitem__(this_that[0].strip().strip(strip_characters), \
+            this_that[1].strip().strip(strip_characters))
+
+        print "Whatever you say, " + sender + "."
+
+    def parse_question(self, mess, must_respond=False):
+        """ Searches the db for a key whose name comes after the 'is'
+        in the message."""
+
+        message = str(mess.getBody())
+        strip_characters = string.punctuation + ' '
+
+        # remove name
+        message_clean = re.sub(self.short_name_regex, '', message)
+
+        # remove everything before trigger statement (what is)
+        message_clean = re.split(self.question_regex, message_clean, maxsplit=1)[2]
+
+        key = message_clean.strip().strip(strip_characters)
+
+        if key in self.keys():
+            print key + ' is ' + self.__getitem__(key)
+        elif must_respond is True:
+            print "I don't know what " + key + " is."
+
 
     # TODO: what if name is stated twice in message
     def callback_message(self, conn, mess):
         message = str(mess.getBody())
-        myFullName = config.BOT_IDENTITY['username']
-        myShortName = myFullName[0:myFullName.find('@')]
 
-        if myFullName in message \
-        or myShortName + ':' in message \
-        or '@' + myShortName in message:
-            print "yes!"
-            self.parseThisThat(message)
+        # is my name mentioned?
+        if re.search(self.short_name_regex, message) != None:
+            # is this a command?
+            if re.search(self.question_regex, message) == None and \
+            re.search(r'\bis\b', message) != None:
+                self.parse_command(mess)
+
+            # is this a request?
+            if re.search(self.question_regex, message) != None:
+                self.parse_question(mess, True)
+
+        else:
+            # is this a request?
+            if re.search(self.question_regex, message) != None:
+                self.parse_question(mess)
 
 #        if emoticonCommand:
 #            key = emoticonCommand.group(1)
